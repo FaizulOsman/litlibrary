ae5b7b0c75d8e33e10206db4731dd0b7cc0cd39f
FaizulOsman <faizulosmanrabby@gmail.com>
2023-09-02 16:40:20 +0600

    feat: zod validation increment for category

--- src/app/modules/category/category.routes.ts
+++ src/app/modules/category/category.routes.ts
@@ -1,7 +1,9 @@
 import { Router } from 'express';
 import { ENUM_USER_ROLE } from '../../../enums/user';
 import auth from '../../middlewares/auth';
+import validateRequest from '../../middlewares/validateRequest';
 import { CategoryController } from './category.controller';
+import { CategoryValidation } from './category.validation';
 
 const router = Router();
 
@@ -9,6 +11,7 @@ const router = Router();
 router.post(
   '/create-category',
   auth(ENUM_USER_ROLE.ADMIN),
+  validateRequest(CategoryValidation.create),
   CategoryController.insertIntoDB
 );
 router.get('/', CategoryController.getAllFromDB);
@@ -16,6 +19,7 @@ router.get('/:id', CategoryController.getByIdFromDB);
 router.patch(
   '/:id',
   auth(ENUM_USER_ROLE.ADMIN),
+  validateRequest(CategoryValidation.update),
   CategoryController.updateIntoDB
 );
 router.delete(
